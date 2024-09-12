3d4d1a4b8baf13e8f3e3e2e597734a4ddf0d305d
Kulbhushan <kulbhushan.mayer@gmail.com>
2024-09-12 09:27:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return """Hello this page has been built using the S2I feature of OpenShift with the name kulpythonapp!
-    Webhook is added in GitHub to trigger Build in Openshift S2I!"""
+    Webhook is added in GitHub to trigger Build in Openshift S2I!
+    Debugging is in progress"""
 
 if __name__ == "__main__":
      app.run(host="0.0.0.0", port=8080)
